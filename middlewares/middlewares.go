b84e5ac8dcbeb53aa669f765c259428edab6f9c9
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-01-20 21:30:25 +0000

    Initial

---
+++ middlewares/middlewares.go
@@ -0,0 +1,42 @@
+// Package middlewares provides common middleware handlers.
+package middlewares
+
+import (
+	"crypto/tls"
+	"github.com/gorilla/context"
+	"github.com/gorilla/sessions"
+	"net/http"
+	"os"
+)
+
+func SetSessionStore(sessionStore sessions.Store) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+			context.Set(req, "sessionStore", sessionStore)
+
+			next.ServeHTTP(res, req)
+		})
+	}
+}
+
+// MustLogin is a middleware that checks existence of current user.
+func MustLogin(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		ciamDomain := os.Getenv("CIAM_DOMAIN")
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: tr}
+		sessionRequest, _ := http.NewRequest("GET", "https://"+ciamDomain+"/ui/api/session/verify", nil)
+		cookie, _ := req.Cookie("token")
+		sessionRequest.AddCookie(cookie)
+		resp, _ := client.Do(sessionRequest)
+
+		if resp == nil || resp.StatusCode != 200 {
+			http.Redirect(res, req, "https://"+ciamDomain, 302)
+			return
+		}
+
+		next.ServeHTTP(res, req)
+	})
+}
