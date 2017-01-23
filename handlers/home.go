0f07c1a6fb28f1dd4cbbc9386a4a69c6c8d44154
Jeff Ploughman <jeff_ploughman@troweprice.com>
2017-01-23 08:36:14 -0500

    Must Login

--- handlers/home.go
+++ handlers/home.go
@@ -1,35 +1,37 @@
 package handlers
 
 import (
+	"crypto/tls"
+	"github.com/Sirupsen/logrus"
 	"github.com/immutability-io/reference-app/libhttp"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	ciamDomain := os.Getenv("CIAM_DOMAIN")
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	sessionDetails, _ := http.NewRequest("GET", "https://"+ciamDomain+"/ui/api/session", nil)
+	cookie, _ := r.Cookie("token")
+	sessionDetails.AddCookie(cookie)
+	resp, _ := client.Do(sessionDetails)
+	var htmlData []byte
+	if resp != nil {
+		htmlData, _ = ioutil.ReadAll(resp.Body)
+		logrus.Debug(string(htmlData))
+	}
 
-	/*
-		ciamDomain := os.Getenv("CIAM_DOMAIN")
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-		client := &http.Client{Transport: tr}
-		sessionDetails, _ := http.NewRequest("GET", "https://"+ciamDomain+"/ui/api/session", nil)
-		cookie, _ := r.Cookie("token")
-		sessionDetails.AddCookie(cookie)
-		resp, _ := client.Do(sessionDetails)
-		var htmlData []byte
-		if resp != nil {
-			htmlData, _ = ioutil.ReadAll(resp.Body)
-			logrus.Debug(string(htmlData))
-		}
-	*/
 	tmpl, err := template.ParseFiles("templates/dashboard.html.tmpl", "templates/home.html.tmpl")
 	if err != nil {
 		libhttp.HandleErrorJson(w, err)
 		return
 	}
 
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, htmlData)
 }
