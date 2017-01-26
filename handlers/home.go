598530f9d9d799fdb252f5774d4f64a142248efe
Jeff Ploughman <jeff_ploughman@troweprice.com>
2017-01-26 15:18:34 -0500

    Vault

--- handlers/home.go
+++ handlers/home.go
@@ -28,6 +28,57 @@ type CIAMSession struct {
 	Customer                 string   `json:"customer"`
 }
 
+type CustomSecret struct {
+	RequestId     string `json:"request_id"`
+	LeaseId       string `json:"lease_id"`
+	Renewable     bool   `json:"renewable"`
+	LeaseDuration int    `json:"lease_duration"`
+	Data          struct {
+		Value struct {
+			Secret1 string `json:"secret1"`
+			Secret2 string `json:"secret2"`
+		}
+		WrapInfo string `json:"wrap_info"`
+		Warnings string `json:"warnings"`
+		Auth     struct {
+			ClientToken string   `json:"client_token"`
+			Accessor    string   `json:"accessor"`
+			Policies    []string `json:"policies"`
+			Meatdata    struct {
+				AuthorityKeyId string `json:"authority_key_id"`
+				CertName       string `json:"cert_name"`
+				CommonName     string `json:"common_name"`
+				SubjectKeyId   string `json:"subject_key_id"`
+			}
+			LeaseDuration int  `json:"lease_duration"`
+			Renewable     bool `json:"renewable"`
+		}
+	}
+}
+
+type AuthenticationResponse struct {
+	RequestId     string `json:"request_id"`
+	LeaseId       string `json:"lease_id"`
+	Renewable     bool   `json:"renewable"`
+	LeaseDuration int    `json:"lease_duration"`
+	Data          string `json:"data"`
+	WrapInfo      string `json:"wrap_info"`
+	Warnings      string `json:"warnings"`
+	Auth          struct {
+		ClientToken string   `json:"client_token"`
+		Accessor    string   `json:"accessor"`
+		Policies    []string `json:"policies"`
+		Meatdata    struct {
+			AuthorityKeyId string `json:"authority_key_id"`
+			CertName       string `json:"cert_name"`
+			CommonName     string `json:"common_name"`
+			SubjectKeyId   string `json:"subject_key_id"`
+		}
+		LeaseDuration int  `json:"lease_duration"`
+		Renewable     bool `json:"renewable"`
+	}
+}
+
 func GetHealth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	tmpl, err := template.ParseFiles("/content/templates/dashboard.html.tmpl", "/content/templates/health.html.tmpl")
@@ -38,6 +89,60 @@ func GetHealth(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmpl.Execute(w, nil)
+}
+
+func GetSecret(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	caCert, err := ioutil.ReadFile("/etc/ssl/root.crt")
+	if err != nil {
+		logrus.Fatal(err)
+		return
+	}
+	certFile := "/etc/ssl/application.crt"
+	keyFile := "/etc/ssl/application.key"
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				Certificates: []tls.Certificate{cert},
+				RootCAs:      caCertPool,
+			},
+		},
+	}
+	authRequest, _ := http.NewRequest("POST", "https://vault.troweprice.com:8200/v1/auth/cert/login", nil)
+	resp, err := client.Do(authRequest)
+	if err != nil {
+		libhttp.HandleErrorJson(w, err)
+		return
+	}
+	var data AuthenticationResponse
+	var htmlData []byte
+	if resp != nil {
+		htmlData, _ = ioutil.ReadAll(resp.Body)
+	}
+	if err != nil {
+		panic(err.Error())
+	}
+	err = json.Unmarshal(htmlData, &data)
+	if err != nil {
+		libhttp.HandleErrorJson(w, err)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("/content/templates/dashboard.html.tmpl", "/content/templates/secret.html.tmpl")
+
+	if err != nil {
+		libhttp.HandleErrorJson(w, err)
+		return
+	}
+
+	tmpl.Execute(w, data)
+
 }
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
