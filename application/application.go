d961cbdaa6d87c5873ec2d6f01e273601efa10ce
Jeff Ploughman <jeff_ploughman@troweprice.com>
2017-01-26 15:21:42 -0500

    Vault

--- application/application.go
+++ application/application.go
@@ -42,6 +42,7 @@ func (app *Application) mux() *gorilla_mux.Router {
 
 	router.Handle("/", middlewares.MustLogin(http.HandlerFunc(handlers.GetHome))).Methods("GET")
 	router.Handle("/health", http.HandlerFunc(handlers.GetHealth)).Methods("GET")
+	router.Handle("/secret", http.HandlerFunc(handlers.GetSecret)).Methods("GET")
 
 	// Path of static files must be last!
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
